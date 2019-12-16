3e935afa4db10e6e0676d50ccf36807b66424993
Alumno <Alumno@DESKTOP-QK63PLK>
2019-12-16 11:10:04 -0500

    envio del 50%

---
+++ src/com/senati/clase01/Prueba.java
@@ -0,0 +1,34 @@
+package com.senati.clase01;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// comentario de linea
+		/*
+		 * programa creado en la ETI
+		 * Autor : jorge antonio
+		 */
+		//long,int,short
+		
+		//Altura de una persona en metros float
+		float altura1=0f;
+		//Altura de una persona en centímetros int 
+		short altura2;
+		//Número del DNI string
+		//Edad de una persona byte
+		//Sexo de una persona char o booleam
+
+	//	System.out.println("altura en metros -->" + altura1);
+		
+		byte x=5;
+		byte y=2;
+		byte aux=0;
+		
+	    aux=y;
+	
+		x=aux;
+		System.out.println("respuesta:" + x);
+	}
+}
+
---
+++ src/com/senati/clase01/ejer3.java
@@ -0,0 +1,10 @@
+package com.senati.clase01;
+
+public class ejer3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
